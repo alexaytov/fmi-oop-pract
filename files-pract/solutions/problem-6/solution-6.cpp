b793b8456b727319fb2d937d707ae014ac910d1e
deyan-deyanov <95212189+deyan-deyanov@users.noreply.github.com>
2025-03-14 15:54:30 +0200

    Update solution-6.cpp

--- files-pract/solutions/problem-6/solution-6.cpp
+++ files-pract/solutions/problem-6/solution-6.cpp
@@ -12,13 +12,20 @@ int main() {
 		return 1;
 	}
 
+	// Поставяме указателя за извличане на потоко в края на файла:
 	fileInputStream.seekg(0, ios::end);
+	// Тъй като указателя е поставен в края на файла и няма повече символи,
+	// функцията tellg() връща броя на символите във файла:
 	cout << "Number of characters in the file: " << fileInputStream.tellg() << endl;
 
+	// Извличаме индекс на символ на файла от конзолата:
+	// ЗАБЕЛЕЖКА! (Файловете, също като масивите започват индексирането си от 0-вата позиция)
 	cout << "Enter symbol index: ";
 	int index;
 	cin >> index;
 
+	// Преместваме указателя за извличане на съответната позиция,
+	// след което извличаме символа на позицията и го отпечатваме на конзолата:
 	fileInputStream.seekg(index, ios::beg);
 	char symbol;
 	fileInputStream.get(symbol);
